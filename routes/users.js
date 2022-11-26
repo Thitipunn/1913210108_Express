e6c4d48fed0c9fe507a469fe2e7f1d74e0ad0837
Thitipunn <da.thitipun_st@tni.ac.th>
2022-11-26 11:31:37 +0700

    workshopA

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Erorr');
+  res.status(200).json({
+    Fullname:"Thitipun Damdenngam"
+  })
+});
+
+router.get('/bio', function(req, res, next) {
+  //res.send('Erorr');
+  res.status(200).json({
+    fullname:"Thitipun Damdenngam",
+    nickname:"Be",
+    hobby:"Reading",
+    gitusername:"Thitipunn"
+  })
 });
 
 module.exports = router;

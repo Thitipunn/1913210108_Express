1ae5c44ebb13b28d942247e7d2316d0c20691b12
Thitipunn <da.thitipun_st@tni.ac.th>
2022-12-10 11:32:54 +0700

    Workshop_A_10/12/2022

--- models/company.js
+++ models/company.js
@@ -6,7 +6,7 @@ const companySchema = new Schema({
   address: {
     province: String,
   }
-});
+},{collection:"setting"});
 
 const companys = mongoose.model('companys',companySchema)
 module.exports = companys;

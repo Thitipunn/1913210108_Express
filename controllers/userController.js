03df95b9b2224b08b01711c01a70da6cb36d62be
Thitipunn <da.thitipun_st@tni.ac.th>
2023-01-14 10:22:26 +0700

    hash Password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require("../models/user")
+
 exports.index = (req, res, next) => {
     //res.send('Erorr');
     res.status(200).json({
@@ -13,3 +15,17 @@ exports.bio = (req, res, next) => {
       gitusername:"Thitipunn"
     })
   }
+exports.register = async (req,res,next) =>{
+  const {name,email,password} = req.body
+
+  let user = new User();
+  user.name = name
+  user.email = email
+  user.password = await user.encryptPassword(password)
+
+  await user.save()
+
+  res.status(201).json({
+    Message:"ลงทะเบียนเรียบร้อยแล้ว"
+  })
+}
